88fbc4f6e574ce7d4eb6db713f031ee629e4983f
Maximiliano Gartner <maxi-gartner@hotmail.com>
2023-01-24 14:30:43 -0300

    :recycle: change js data for function that can be reused

--- data/data.js
+++ data/data.js
@@ -1,4 +1,4 @@
-const buttonSport = document.getElementById('buttom-sports');
+/* const buttonSport = document.getElementById('buttom-sports');
 const infoSport = document.getElementById('info-sport');
 const infoSportShort = document.getElementById('info-sport-short');
 
@@ -11,8 +11,9 @@ const infoSportShort = document.getElementById('info-sport-short');
         }else{
             infoSport.style.display = 'none';
             infoSportShort.style.display = 'block';
-            buttonSport.style.color = 'black';
-            buttonSport.style.background = 'white';
+            buttonSport.style.color = 'goldenrod';
+            buttonSport.style.background = '#00000083';
+            buttonSport.style.border = 'solid 2px goldenrod';
         }
     })
 
@@ -29,8 +30,9 @@ const infoRegulationShort = document.getElementById('info-regulation-short')
         }else{
             infoRegulation.style.display = 'none';
             infoRegulationShort.style.display = 'block';
-            buttonRegulation.style.color = 'black';
-            buttonRegulation.style.background = 'white';
+            buttonRegulation.style.color = 'goldenrod';
+            buttonRegulation.style.background = '#00000083';
+            buttonRegulation.style.border = 'solid 2px goldenrod';
         }
     })
 
@@ -47,9 +49,52 @@ buttonallPlay.addEventListener('click', () =>{
         }else{
             infoallPlay.style.display = 'none';
             infoallPlayShort.style.display = 'block';
-            buttonallPlay.style.color = 'black';
-            buttonallPlay.style.background = 'white';
+            buttonallPlay.style.color = 'goldenrod';
+            buttonallPlay.style.background = '#00000083';
+            buttonallPlay.style.border = 'solid 2px goldenrod';
         }
     })
 
     console.log("hasta aca funciona")
+ */
+
+    const btn = document.querySelectorAll('.btn');
+    console.log(btn)
+    const infoBtnShort = document.querySelectorAll('.infoBtnShort');
+    console.log(infoBtnShort)
+    const infoBtn = document.querySelectorAll('.infoBtn');
+    console.log(infoBtn)
+
+    btn.forEach(button =>{
+        button.addEventListener('click', e =>{
+            btn.forEach(button =>{
+                if(button.id == e.target.id){
+                    button.style.color = 'white';
+                    button.style.background = 'green';
+                    button.style.border = 'solid 2px goldenrod';
+                }else{
+                    button.style.color = 'goldenrod';
+                    button.style.background = '#00000083';
+                    button.style.border = 'solid 2px goldenrod';
+                }
+            })
+            infoBtn.forEach((info) =>{
+                if(info.style.display == 'none'){
+                    if(info.id == e.target.id){
+                        info.style.display = 'block';
+                    }
+                    }else{
+                        info.style.display = 'none';
+                }
+            })
+            infoBtnShort.forEach((infoShort) =>{
+                if(infoShort.style.display == 'block'){
+                    if(infoShort.id == e.target.id){
+                        infoShort.style.display = 'none';
+                    }
+                    }else{
+                        infoShort.style.display = 'block';
+                }
+            })
+        })
+    })
